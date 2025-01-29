60a283d15d83bd0397b7246c55fb9a463e7cd598
Justin Bishop <mail@dissoc.me>
2025-01-29 16:33:13 -0600

    client: enable forwarding, session: create session

--- src/ssh_clj/client.clj
+++ src/ssh_clj/client.clj
@@ -1,11 +1,16 @@
 (ns ssh-clj.client
+  (:require
+   [clojure.java.io :as io]
+   [ssh-clj.forwarding :refer [forwarding-filter-all]])
   (:import
    (java.time Duration)
    (org.apache.sshd.client SshClient)
+   [org.apache.sshd.client.session ClientSession]
+   (org.apache.sshd.common.keyprovider KeyIdentityProvider)
+   (org.apache.sshd.common.util.security SecurityUtils)
    (org.apache.sshd.core CoreModuleProperties)))
 
-
-(defn create-client
+(defn ^SshClient create-client
   [& {:keys [enable-forwarding? keepalive-interval]
       :or   {enable-forwarding  false
              keepalive-interval 30}}]
@@ -14,5 +19,37 @@
       (.set CoreModuleProperties/HEARTBEAT_INTERVAL
             client
             (Duration/ofSeconds keepalive-interval)))
+    (when enable-forwarding?
+      (.setForwardingFilter client (forwarding-filter-all)))
     (.start client)
     client))
+
+
+(defn load-key-pair-identities [key-pair-path]
+  (let [input-stream (-> key-pair-path
+                         io/file
+                         io/input-stream)]
+    (SecurityUtils/loadKeyPairIdentities nil nil input-stream nil)))
+
+;; TODO handle more key types
+(defn ^ClientSession create-session
+  [client host port
+   & {:keys [user key-path keepalive-interval connect-timeout]
+      :or   {key-path           (str (System/getProperty "user.home")
+                                     "/.ssh/id_ed25519")
+             user               (System/getProperty "user.name")
+             keepalive-interval 30
+             connect-timeout    30}}]
+  (let [session (-> (.connect client user host port)
+                    (doto
+                        (.verify (* 1000 connect-timeout)))
+                    .getSession)
+
+        key-pair (load-key-pair-identities key-path)]
+
+    (->> (KeyIdentityProvider/wrapKeyPairs key-pair)
+         (.setKeyIdentityProvider session))
+
+    (-> (.auth session)
+        (.verify (* 1000 connect-timeout)))
+    session))
